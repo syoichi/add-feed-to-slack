3d5d30b8ac74afd172e3544ea969ecccb02572e0
Syoichi Tsuyuhara <syoichi@outlook.com>
2016-12-12 19:23:08 +0900

    Refactor content.js

--- js/content.js
+++ js/content.js
@@ -3,10 +3,10 @@
 try {
   chrome.runtime.sendMessage(null, {
     url: (new URL(
-      document.querySelector([
-        'link[rel="alternate"][type="application/rss+xml"][href]',
-        'link[rel="alternate"][type="application/atom+xml"][href]'
-      ].join(',')).href
+      document.querySelector(`
+        link[rel="alternate"][type="application/rss+xml"][href],
+        link[rel="alternate"][type="application/atom+xml"][href]
+      `).href
     )).toString()
   });
 } catch (err) {
